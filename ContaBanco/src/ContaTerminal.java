0dc6d3cad40160ff3c70613e5a0d5f5b217a93e1
Christyan Campos <christyancampos@hotmail.com>
2024-04-18 21:03:18 -0300

    Add DecimalFormat para formatar o saldo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -7,23 +7,23 @@ public class ContaTerminal {
         Scanner scn = new Scanner(System.in);
         String nome;
         String agencia;
-        int conta;
         double saldo;
+        int conta;
 
         System.out.println("Por favor, digite o seu nome:");
         nome = scn.nextLine(); 
-
+        
+        System.out.println("Digite a agência:");
+        agencia = scn.nextLine();
+        
         System.out.println("Digite o número da conta:");
         conta = scn.nextInt();
 
-        System.out.println("Digite a agência:");
-        agencia = scn.nextLine();
-
         System.out.println("Digite seu saldo:");
         saldo = scn.nextDouble();
-        scn.close();
+        scn.close();    
 
         System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " 
-            + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+            + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");    
     }
 }
